1f17d45de2752cfd6619333220a0bd02fd57e625
rickydomingos97 <ricardodomingos97@gmail.com>
2021-02-14 11:52:16 -0300

    java dia 3 alura

--- JavaAlura/Startjava.java
+++ JavaAlura/Startjava.java
@@ -1,6 +1,5 @@
 public class Startjava{
-    public static void main(String[] args){
-        System.out.println("Mais uma tentativa de escrever um codigo java");
-        System.out.println("Isso eh mais do que necessario");
+    public static void main(String[]args){
+        System.out.println("Ola pessoas");
     }
 }
