2149e7423a1559c725318b34c11395041caa2b82
rickydomingos97 <ricardodomingos97@gmail.com>
2021-02-20 18:48:36 -0300

    metodo transfere iniciado

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -5,9 +5,24 @@ public class Conta {
     String titular;
 
     // METODO EM JAVA //
+    // DEPOSITO NA CONTA //
     public void deposita(double valor){
     this.saldo += + valor;
     }
+    // SAQUE NA CONTA //
+    public boolean saca(double valor) {
+    	if(this.saldo >= valor) {
+    		this.saldo -= valor;
+            System.out.println("voce retirou!");
+    		return true;
+    	} else {
+            System.out.println("voce nao tem saldo");
+            System.out.println("O seu saldo eh: " + saldo);
+    		return false;
+    	}
+    }
+
+    
 }
 
 // ISSO AINDA NAO EH UMA CONTA //
--- bytebank/src/TestaMetodo.java
+++ bytebank/src/TestaMetodo.java
@@ -3,9 +3,17 @@ public class TestaMetodo {
     public static void main(String[] args) {
         Conta contaDoRicky = new Conta();
         contaDoRicky.saldo = 100;
-        contaDoRicky.deposita( 50);
+        contaDoRicky.deposita(150);
+        // contaDoRicky.saca(1330);
 
-        System.out.println("O VALOR DO SALDO EM CONTA EH DE: " + contaDoRicky.saldo);
-        System.out.println();
+
+        boolean conseguiuRetirar = contaDoRicky.saca(1113);
+        System.out.println(contaDoRicky.saldo);
+        System.out.println(conseguiuRetirar);
+
+        Conta contaDaIrina = new Conta();
+        contaDaIrina.deposita(1000);
+// Nao precisa usar o this aqui, por que ja pasasmos a origem contaDaIrina //
+        contaDaIrina.tranfere(300, contaDoRicky);
     }
 }
